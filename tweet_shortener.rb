79490f848c1932c4b4e601fffad5cafbc2ff5a60
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-22 19:38:40 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,8 +16,8 @@ def word_substituter(string)
   dictionary.each do |word, replace|
     array.each do |it|
       if word.include?(it)
-        str += replace
-      else str += it
+        str += "#{replace} "
+      else str += "#{it}""
       end
     end
   end

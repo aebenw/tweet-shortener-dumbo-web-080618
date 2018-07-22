04e33ae810dd20e64c8c8bbbd0e5e307f8772d24
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-22 19:33:30 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,8 +1,8 @@
 def dictionary
   shortener = {
   "hello" => 'hi',
-["to, two, too"] => '2', 
-["for, four"] => '4',
+"to, two, too" => '2', 
+"for, four" => '4',
 'be' => 'b',
 'you' => 'u',
 "at" => "@",
@@ -14,7 +14,6 @@ def word_substituter(string)
   array = string.split(" ")
   dictionary.each do |word, replace|
     array.each do |it|
-      word_arr = word.join(", ")
       if it.include?(word)
         it = replace
       end
